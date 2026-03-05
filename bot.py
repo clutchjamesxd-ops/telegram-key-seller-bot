3ea1edbd762eca4a8a034641e0e357b2a9dfff11
clutchjamesxd-ops <clutchjamesxd@gmail.com>
2026-03-06 03:34:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+TOKEN = "8758244481:AAGgx1ZmC7blLY2Ll7Hu6N66G9HsR4LlE3Y"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Welcome to Key Seller Bot 🔥")
